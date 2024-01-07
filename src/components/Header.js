58279f933fddf36b34ae3201c3476ca0fca9c5b2
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2024-01-07 09:25:37 +0100

    Our first commit.

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="fixed shadow-md w-full h-16 px-2 md:px-4">
+      <div className=" flex item-center h-full">
+        <div className="h-12">Book-shop</div>
+      </div>
+    </header>
+  );
+}
